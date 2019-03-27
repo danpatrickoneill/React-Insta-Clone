dd803901a8eda7f942a4f597a4b421edae60a1c0
danpatrickoneill <dan47573@gmail.com>
2019-03-27 15:30:24 -0400

    Refactored app into PostsPage component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 
-import SearchBar from "./components/SearchBar/SearchBar";
-import PostContainer from "./components/PostContainer/PostContainer";
-import CommentSection from "./components/CommentSection/CommentSection";
+// import SearchBar from "./components/SearchBar/SearchBar";
+// import PostContainer from "./components/PostContainer/PostContainer";
+// import CommentSection from "./components/CommentSection/CommentSection";
+import PostsPage from "./components/PostContainer/PostsPage";
 
 import dummyData from "./dummy-data";
 import "./App.css";
@@ -11,70 +11,13 @@ import "./App.css";
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      data: [],
-      commentID: 14
-    };
+    this.state = {};
   }
-
-  componentDidMount() {
-    this.setState({
-      data: dummyData
-    });
-  }
-
-  searchPosts = text => {
-    if (text) {
-      this.setState({
-        data: this.state.data.filter(post => post.username.includes(text))
-      });
-    } else {
-      this.setState({
-        data: dummyData
-      });
-    }
-  };
-
-  appendComment = (text, id) => {
-    const newComment = {
-      username: "doneill",
-      id: this.state.commentID.toString(),
-      text: text
-    };
-    let newID = this.state.commentID + 1;
-    this.setState({
-      commentID: newID
-    });
-    this.setState({
-      data: this.state.data.map(post => {
-        if (post.id === id) {
-          post.comments.push(newComment);
-        }
-        return post;
-      })
-    });
-  };
-
-  addLike = id => {
-    this.setState({
-      data: this.state.data.map(post => {
-        if (post.id === id) {
-          post.likes += 1;
-        }
-        return post;
-      })
-    });
-  };
 
   render() {
     return (
       <div className="App">
-        <SearchBar data={this.state.data} searchPosts={this.searchPosts} />
-        <PostContainer
-          data={this.state.data}
-          appendComment={this.appendComment}
-          addLike={this.addLike}
-        />
+        <PostsPage />
       </div>
     );
   }
